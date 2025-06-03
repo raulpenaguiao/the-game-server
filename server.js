b107f81c1f4fece87b99e73659225b1dcd7774e6
raulpenaguiao <92503059+raulpenaguiao@users.noreply.github.com>
2025-06-03 12:53:22 +0200

    player tab

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+
 // Player Page
 app.get('/player/:playerstring', (req, res) => {
     console.log('Player string:', req.params.playerstring);
